0d14e1eee4d1eec1bd1dc2d5f730181eb85322ca
Waikikec <martin.d.yankov@gmail.com>
2023-11-07 13:51:23 +0200

    feat: add Post api

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 async function getData(id) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: 'no-store',
   });
 
@@ -23,74 +23,24 @@ const BlogPost = async ({ params }) => {
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>{data.body}</p>
+          <p className={styles.desc}>{data.description}</p>
           <div className={styles.author}>
             <Image
-              src='https://images.pexels.com/users/avatars/170195/icons8-509.png?auto=compress&fit=crop&h=50&w=50&dpr=1'
+              src={data.image}
               alt=''
               width={40}
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>Martin</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/1616403/pexels-photo-1616403.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-            alt=''
-            fill={true}
-            className={styles.image}
-          />
+          <Image src={data.image} alt='' fill={true} className={styles.image} />
         </div>
       </div>
       <div className={styles.content}>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam
-          ipsa dicta aliquid architecto deleniti rem quidem vel sunt, similique
-          soluta excepturi totam sapiente expedita omnis earum magnam unde sit
-          voluptate quam iusto magni repellat eaque! Alias suscipit vero quam
-          perspiciatis? Sequi ratione officiis eum aperiam inventore voluptate
-          voluptatum sit deleniti eligendi neque provident repellendus natus
-          officia id, delectus, reiciendis quia mollitia odit. Quis repellat
-          excepturi velit eum error praesentium nesciunt ipsa vel. Voluptate
-          quaerat vel laudantium doloribus maxime, molestias consequatur nam sit
-          et minima molestiae reprehenderit architecto, labore rem velit?
-          Aliquam aspernatur placeat modi doloremque nam soluta esse doloribus
-          eum tenetur voluptates vel, laudantium eius aliquid velit, eveniet
-          voluptatibus similique repellendus? Atque, commodi libero sunt maiores
-          eligendi odio eum, sed quo aperiam voluptatibus, animi tenetur quae
-          voluptatum nesciunt! A ex ratione optio excepturi consequuntur vel
-          quaerat fugiat modi quam accusamus voluptate temporibus, nulla,
-          tempore doloribus sit repellat voluptatibus praesentium nihil,
-          quisquam sequi earum recusandae nobis ipsam. Nobis corrupti rerum
-          fuga! Consequatur quam suscipit neque est tenetur, dolorum itaque ipsa
-          earum fugiat voluptates dolores perspiciatis libero illum! Sed omnis
-          exercitationem inventore ratione culpa aspernatur fugiat accusantium
-          numquam vero, nam, nemo, atque id. Animi quaerat perspiciatis adipisci
-          esse aut, architecto sit a soluta harum, facere, ex excepturi ullam!
-          Quibusdam consequuntur, ipsum vitae laudantium, voluptatem omnis
-          quaerat asperiores at, aperiam amet sed? Ipsam dignissimos non
-          perspiciatis, laboriosam molestias repellat facere asperiores ratione
-          aliquam, quas veniam esse tempora, nisi ipsa! Molestiae sapiente
-          reiciendis id ex ut eligendi quisquam asperiores voluptate quae
-          deleniti voluptates cum modi, porro itaque blanditiis amet. Voluptatum
-          officia, natus laboriosam modi recusandae sapiente reprehenderit
-          possimus, hic tempore soluta ratione architecto tenetur inventore,
-          delectus laborum id ea. Distinctio perferendis veniam nemo cupiditate
-          corporis quidem voluptate provident quibusdam labore libero autem,
-          cumque, temporibus nesciunt quo repudiandae sunt? In, mollitia
-          laudantium delectus aperiam ex quod voluptas assumenda, debitis
-          suscipit consequatur, beatae aut similique maxime eligendi cupiditate
-          ratione? Recusandae alias repellat fuga voluptatem tempora maxime
-          velit dolores quaerat sapiente, aliquid iusto architecto? Voluptatibus
-          asperiores soluta enim nulla alias illo illum assumenda mollitia
-          provident facere, quas quibusdam tempore dignissimos? Asperiores
-          doloremque nulla corporis, architecto, veniam ab sunt reiciendis dolor
-          qui nobis eveniet, accusamus totam odit perspiciatis quo? Quisquam
-          ratione neque minus, quasi quidem voluptas animi sapiente numquam,
-          quos aperiam possimus eius impedit maxime.
-        </p>
+        <p className={styles.text}>{data.content}</p>
       </div>
     </div>
   );
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -29,7 +29,13 @@ const Category = ({ params }) => {
             <Button text='See More' url='#' />
           </div>
           <div className={styles.imgContainer}>
-            <Image className={styles.img} fill={true} src={item.image} alt='' />
+            <Image
+              className={styles.img}
+              fill={true}
+              src={item.image}
+              alt=''
+              sizes='(max-width: 768px) 100vw'
+            />
           </div>
         </div>
       ))}
