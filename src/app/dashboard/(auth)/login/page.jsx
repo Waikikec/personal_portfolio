980c651572c77fed27c10ed5106e38f3be4de692
Waikikec <martin.d.yankov@gmail.com>
2023-11-07 17:14:15 +0200

    feat: add login/register pages

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -3,9 +3,33 @@ import { signIn } from 'next-auth/react';
 import React from 'react';
 
 const Login = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn('credentials', { email, password });
+  };
+
   return (
     <div>
-      <button onClick={() => signIn('google')}>Login with Google</button>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type='email'
+          placeholder='email'
+          className={styles.input}
+          required
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className={styles.input}
+          required
+        />
+        <button className={styles.button}>Login</button>
+        <button onClick={() => signIn('google')}>Login with Google</button>
+      </form>
     </div>
   );
 };
